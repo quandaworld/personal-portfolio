2218ad0878defd9171d9371c34b803c27ec08fea
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-01-20 00:57:46 -0800

    Add portfolio data

---
+++ data/portfolio.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    title: 'Personal Portfolio',
+    imgUrl: '',
+    stack: ['React', 'Tailwind', 'Vite'],
+    link: 'https://github.com/quandaworld',
+  },
+  {
+    title: 'Tic-Tac-Toe with Unbeatable AI',
+    imgUrl: 'src/assets/tic-tac-toe.png',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    link: 'https://quandaworld.github.io/tic-tac-toe/',
+  },
+  {
+    title: 'Book Library',
+    imgUrl: 'src/assets/book-library.png',
+    stack: ['HTML', 'CSS', 'JavaScript', 'Firebase'],
+    link: 'https://github.com/quandaworld/library/',
+  },
+  {
+    title: 'Calculator',
+    imgUrl: 'src/assets/calculator.png',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    link: 'https://github.com/quandaworld/calculator',
+  },
+  {
+    title: 'Etch A Sketch',
+    imgUrl: 'src/assets/etch-a-sketch.png',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    link: 'https://quandaworld.github.io/etch-a-sketch/',
+  },
+  {
+    title: 'Rock Paper Scissors',
+    imgUrl: 'src/assets/rock-paper-scissors.png',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    link: 'https://github.com/quandaworld/rock-paper-scissors',
+  },
+]
