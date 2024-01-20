d34725c51bc2b75065d92b20be9255111eb20b9d
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-01-20 00:59:52 -0800

    Update intro content

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Quan Nguyen</h1>
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Quan N. Nguyen</h1>
       <p className="text-base md:text-xl mb-3 font-medium">Self-Taught Software Engineer</p>
-      <p className="text-sm max-w-xl mb-2 font-bold">As a self-taught developer with a passion for technology, I am eager to further expand my expertise and contribute to innovative projects in the world of software and mobile application development. As a former tax accountant, I am highly detail-oriented, organized, and have a logical mindset, which allows me to approach programming challenges in a systematic and efficient manner. My keen interest in problem-solving drives me to constantly acquire new skills and knowledge, making me a valuable addition to any development team.<br />
-      I am seeking opportunities to learn and grow in the field of software and mobile application development, and am committed to making meaningful contributions to cutting-edge projects. Let's connect and explore how we can work together to create innovative solutions with lasting impacts.</p>
+      <p className="text-sm max-w-xl mb-2 font-bold">As a self-taught developer with a passion for technology, I am eager to further expand my expertise and contribute to innovative projects in the world of software and mobile application development. As a former tax accountant, I am highly detail-oriented, organized, and have a logical mindset, which allows me to approach programming challenges in a systematic and efficient manner. My keen interest in problem-solving drives me to constantly acquire new skills and knowledge, making me a valuable addition to any development team.</p>
+      <p className='text-sm max-w-xl mb-2 font-bold'>I am seeking opportunities to learn and grow in the field of software and mobile application development, and am committed to making meaningful contributions to cutting-edge projects.</p>
     </div>
   )
 }
