899915b2d8098d84e58892c2b1a5678e3d2e03ab
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-01-21 15:38:49 -0800

    Add timeline dada file

---
+++ data/timeline.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    year: 'Today',
+    title: 'Full Stack Web Developer - Freelancing',
+    duration: 'July 2023 - Present',
+    details: 'To be updated',
+  },
+  {
+    year: '2022',
+    title: 'Self-Taught Programming',
+    duration: '3 years',
+    details: 'To be updated',
+  },
+  {
+    year: '2021',
+    title: 'First Line of Code',
+    duration: 'the beginning',
+    details: 'To be updated',
+  },
+  {
+    year: '2018',
+    title: 'Senior Business Tax Associate',
+    duration: '3 years',
+    details: 'To be updated',
+  },
+  {
+    year: '2014',
+    title: "Bachelor's Degree in Accounting",
+    duration: '4 years',
+    details: 'To be updated',
+  },
+]
