95360fcaa68713a12b2259f440e6098b6efcdfc6
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-01-21 19:21:44 -0800

    Import timeline item data and styling to timeline component

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import timeline from '../../data/timeline';
+import TimelineItem from './TimelineItem';
 
 function Timeline() {
   return (
-    <div>Timeline</div>
+    <div className='flex flex-col md:flex-row justify-center my-20'>
+      <div className='w-full md:w-7/12'>
+        {timeline.map(item => (
+            <TimelineItem
+              year={item.year}
+              title={item.title}
+              duration={item.duration}
+              details={item.details}
+            />
+        ))}
+      </div>
+    </div>
   )
 }
 
