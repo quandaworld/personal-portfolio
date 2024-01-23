01f96952ba979ecb9da4b41de1f971db103ebf8a
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-01-23 12:38:49 -0800

    Update dark theme color

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
       >
         {theme === 'dark' ? sun : moon}
       </button>
-      <div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
+      <div className="bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-300 min-h-screen font-inter">
         <div className='max-w-5xl w-11/12 mx-auto'>
           <Intro />
           <Portfolio />
--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 function TimelineItem({ year, title, duration, details }) {
   return (
-    <ol className='flex flex-col md:flex-row relative border-l border-stone-200 dark:border-stone-700'>
+    <ol className='flex flex-col md:flex-row relative border-l border-gray-200 dark:border-gray-700'>
       <li className='mb-10 ml-4'>
-        <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-stone-900 dark:bg-stone-700"/>
+        <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-800 dark:bg-gray-700"/>
         <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
-          <span className='inline-block px-2 py-1 font-semibold text-white dark:text-stone-900 bg-stone-900 dark:bg-white rounded-md'>
+          <span className='inline-block px-2 py-1 font-semibold text-white dark:text-gray-800 bg-gray-800 dark:bg-white rounded-md'>
             {year}
           </span>
-          <h3 className='text-lg font-semibold text-stone-900 dark:text-white'>
+          <h3 className='text-lg font-semibold text-gray-800 dark:text-white'>
             {title}
           </h3>
-          <div className="my-1 text-sm font-normal leading-none text-stone-400 dark:text-stone-500">
+          <div className="my-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             {duration}
           </div>
         </p>
-        <p className='my-2 text-base font-normal text-stone-500 dark:text-stone-400'>
+        <p className='my-2 text-base font-normal text-gray-500 dark:text-gray-400'>
           {details}
         </p>
       </li>
