501b01fc9af296ddfc9ea3e41d652e0fd1269869
quandaworld <nhiquannguyen@gmail.com>
2025-03-10 17:52:27 -0700

    Update projects listing styling

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -5,18 +5,18 @@ function PortfolioItem({ title, imgUrl, stack, link }) {
     <a href={link}
         target='_blank'
         rel='noopener noreferrer'
-        className='border-2 border-gray-900 dark:border-white rounded-md overflow-hidden'
+        className='border-[1.5px] border-gray-500 dark:border-amber-50 rounded-md overflow-hidden'
     >
       <img
         src={imgUrl}
         alt="portfolio"
-        className="w-full h-36 md:h-48 object-cover cursor-pointer"
+        className="w-full h-auto md:h-48 object-cover cursor-pointer"
       />
       <div className="w-full p-4">
-        <h3 className='text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold'>{title}</h3>
-        <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white'>
+        <h3 className='text-lg md:text-xl dark:text-amber-50 mb-2 md:mb-3 font-semibold'>{title}</h3>
+        <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-amber-50'>
           {stack.map(item => (
-            <span className='inline-block px-2 py-1 font-semibold border-2 border-gray-900 dark:border-white rounded-md'>
+            <span className='inline-block px-2 py-1 font-semibold border-[1.5px] border-gray-500 dark:border-amber-50 rounded-md'>
               {item}
             </span>
           ))}
