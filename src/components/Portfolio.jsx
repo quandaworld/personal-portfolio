52cb9b2a531bf4d17367af0a264409e5a0c58011
quandaworld <nhiquannguyen@gmail.com>
2025-03-22 20:38:30 -0700

    Implement github and deployed site links in portfolio and portfolio items

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,12 +6,14 @@ function Portfolio() {
   return (
     <div className="flex flex-col md:flex-row items-center justify-center">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {portfolio.map(project => (
+        {portfolio.map((project, index) => (
           <PortfolioItem 
+            key={index}
             imgUrl={project.imgUrl}
             title={project.title}
             stack={project.stack}
             link={project.link}
+            github={project.github}
           />
         ))}
       </div>
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,28 +1,46 @@
 import React from 'react';
 
-function PortfolioItem({ title, imgUrl, stack, link }) {
+function PortfolioItem({ title, imgUrl, stack, link, github }) {
   return (
-    <a href={link}
-        target='_blank'
-        rel='noopener noreferrer'
-        className='border-[1.5px] border-gray-500 dark:border-amber-50 rounded-md overflow-hidden'
-    >
+    <div className='border-[1.5px] border-gray-500 dark:border-amber-50 rounded-md overflow-hidden'>
       <img
         src={imgUrl}
         alt="portfolio"
         className="w-full h-auto md:h-48 object-cover cursor-pointer"
       />
       <div className="w-full p-4">
-        <h3 className='text-lg md:text-xl dark:text-amber-50 mb-2 md:mb-3 font-semibold'>{title}</h3>
+        <div className="flex justify-between items-center mb-2 md:mb-3">
+          <h3 className='text-lg md:text-xl dark:text-amber-50 font-semibold'>{title}</h3>
+          <div className="flex space-x-2">
+            <a href={link} target='_blank' rel='noopener noreferrer'>
+              <img 
+                src="/assets/deployedSite.svg" 
+                alt="Deployed Site" 
+                className="dark:filter dark:brightness-0 dark:invert-[0.85] dark:sepia-[0.20] dark:saturate-[1000%] dark:hue-rotate-[340deg]"
+                style={{ width: '1.2rem', height: '1.4rem' }} 
+              />
+            </a>
+            {github && (
+              <a href={github} target='_blank' rel='noopener noreferrer'>
+                <img 
+                  src="/assets/github.svg" 
+                  alt="Github Repo" 
+                  className="dark:filter dark:brightness-0 dark:invert-[0.85] dark:sepia-[0.20] dark:saturate-[1000%] dark:hue-rotate-[340deg]"
+                  style={{ width: '1.3rem', height: '1.33rem' }} 
+                />
+              </a>
+            )}
+          </div>
+        </div>
         <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-amber-50'>
-          {stack.map(item => (
-            <span className='inline-block px-2 py-1 font-semibold border-[1.5px] border-gray-500 dark:border-amber-50 rounded-md'>
+          {stack.map((item, index) => (
+            <span key={index} className='inline-block px-2 py-1 font-semibold border-[1.5px] border-gray-500 dark:border-amber-50 rounded-md'>
               {item}
             </span>
           ))}
         </p>
       </div>
-    </a>
+    </div>
   )
 }
 
